8d4cec873f46f4aaad4c5cb30529a5ad83cd4e5e
Brandon Andreis <bjandreis@gmail.com>
2020-08-18 16:48:02 -0400

    Fixed the welcome screen to not show the welcome message twice. Show a 404 error when the page doesn't exist.

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import Welcome from './components/welcome/Welcome'
 import Clock from './components/clock/Clock'
 import Contact from './components/contact/Contact'
 import Navigation from './components/navigation/Navigation'
+import Error from './components/error/Error'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Route
-        exact
-        path="/"
-        render={(props) => <Welcome {...props} name="Brandon" />}
-      />
-      <Route path="/welcome" component={Welcome} />
-      <Route path="/clock" component={Clock} />
-      <Route path="/contact" component={Contact} />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Brandon" />}
+        />
+        <Route path="/welcome/:name" render={(props) => <Welcome {...props} name={props.match.params.name} />} />
+        <Route path="/clock" component={Clock} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navigation(props) {
     return (
@@ -9,21 +9,8 @@ function Navigation(props) {
                 <li><Link to="/clock">Clock</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
             </ul>
-            <Switch>
-                <Route path="/welcome/:name" children={<Child />} />
-            </Switch>
         </div>
     );
 }
 
 export default Navigation;
-
-function Child() {
-    let { name } = useParams();
-  
-    return (
-      <div>
-        <p>Welcome, {name}!</p>
-      </div>
-    );
-  }
